e15606f58c336845227af6afa90375b8f818c2ee
Muhammad Fadhlika <Muhammad Fadhlika>
2016-08-27 09:08:42 +0700

    Use webview & Add modul 1 dasar teori

--- app/src/main/java/xyz/fadh/praktikumelektronika/DetailActivity.java
+++ app/src/main/java/xyz/fadh/praktikumelektronika/DetailActivity.java
@@ -3,19 +3,31 @@ package xyz.fadh.praktikumelektronika;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
-import com.mukesh.MarkdownView;
 
 public class DetailActivity extends AppCompatActivity {
 
+    WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
         Intent intent = getIntent();
-        int noModule = getIntent().getIntExtra("module", 0);
+        int noModule = getIntent().getIntExtra("module", 0) + 1;
+        String nameModule = getIntent().getStringExtra("name");
 
+        getSupportActionBar().setTitle("Modul " + noModule + " " + nameModule);
 
+        webView = (WebView) findViewById(R.id.wvContent);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setBuiltInZoomControls(false);
+        webSettings.setJavaScriptEnabled(false);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/modul1/modul1.html");
     }
 }
